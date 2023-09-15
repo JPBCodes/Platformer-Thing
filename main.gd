6286d776fe20b7266a9f494dce72ec404c67b8ca
Justin Bertrand <52991734+jpbaz28@users.noreply.github.com>
2023-09-15 14:23:58 -0700

    first commit

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _ready():
+	Utils.saveGame()
+	Utils.loadGame()
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
+
+func _on_play_btn_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
